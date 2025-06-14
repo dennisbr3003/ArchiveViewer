160ac3b4a40accf5f19e7105e1da7d5c6a719e4c
Dennis Brink <dennis.mjknd@gmail.com>
2025-06-14 22:57:17 +0200

    Converted app to fragments. Structured classes

--- app/src/main/java/com/dennisbrink/mt/global/mypackedfileviewer/ImageAdapter.java
+++ app/src/main/java/com/dennisbrink/mt/global/mypackedfileviewer/ImageAdapter.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,7 +14,9 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.signature.ObjectKey;
@@ -43,11 +46,13 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         PhotoView photoView; // https://www.geeksforgeeks.org/photoview-in-android/
+        ImageView imageView;
         public TextView fileNameView, fileExtensionView, fileDateView, fileSizeView, filePositionView;
 
         public ViewHolder(View view) {
             super(view);
             photoView = view.findViewById(R.id.photoView);
+            imageView = view.findViewById(R.id.imageViewGif);
             fileNameView = view.findViewById(R.id.fileNameView);
             fileExtensionView = view.findViewById(R.id.fileExtensionView);
             fileDateView = view.findViewById(R.id.fileDateView);
@@ -69,7 +74,7 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
 
         ZipEntryData entryData = zipEntries.get(position);
 
-        Log.d("DB1", "load file " + entryData.getFileName());
+        Log.d("DB1", "ImageAdapter.onBindViewHolder: load file " + entryData.getFileName());
 
         InputStream inputStream = null;
         try {
@@ -78,7 +83,9 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
             throw new RuntimeException(e);
         }
 
-        holder.fileExtensionView.setText(ZipUtilities.getFileExtension(entryData.getFileName()));
+        String fileExtension =  ZipUtilities.getFileExtension(entryData.getFileName());
+
+        holder.fileExtensionView.setText(fileExtension);
         holder.fileNameView.setText(entryData.getFileName());
         holder.fileDateView.setText(entryData.getDisplayDateTime());
         holder.fileSizeView.setText(entryData.getDisplaySize());
@@ -86,16 +93,28 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
 
         Glide.with(holder.photoView.getContext()).clear(holder.photoView);
 
-        Glide.with(holder.photoView.getContext())
-                .load(inputStream)
-                .listener(new GlideRequestListener())
-                //.placeholder(R.drawable.transparent_placeholder) // no placeholder
-                .error(R.drawable.no_image)
-                .signature(new ObjectKey(UUID.randomUUID().toString())) // use a signature and keep using cache without the mix-ups
-                //.diskCacheStrategy(DiskCacheStrategy.NONE) // Disable disk caching
-                //.skipMemoryCache(true) // Disable memory caching
-                .into(holder.photoView);
-
+        if(!fileExtension.equals("GIF")) {
+            Log.d("DB1", "ImageAdapter.onBindViewHolder: use PhotoView");
+            holder.photoView.setVisibility(View.VISIBLE);
+            holder.imageView.setVisibility(View.INVISIBLE);
+            Glide.with(holder.photoView.getContext())
+                    .load(inputStream)
+                    .listener(new GlideRequestListener())
+                    .error(R.drawable.no_image)
+                    .signature(new ObjectKey(UUID.randomUUID().toString())) // use a signature and keep using cache without the mix-ups
+                    .into(holder.photoView);
+        } else {
+            Log.d("DB1", "ImageAdapter.onBindViewHolder: use ImageView");
+            holder.photoView.setVisibility(View.INVISIBLE);
+            holder.imageView.setVisibility(View.VISIBLE);
+            Glide.with(holder.photoView.getContext())
+                    .asGif()
+                    .load(inputStream)
+                    .listener(new GlideGifRequestListener())
+                    .error(R.drawable.no_image)
+                    .signature(new ObjectKey(UUID.randomUUID().toString())) // use a signature and keep using cache without the mix-ups
+                    .into(holder.imageView);
+        }
     }
 
     @Override
@@ -123,26 +142,32 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
         return zipFile.getInputStream(fileHeader);
     }
 
+    private static class GlideGifRequestListener implements RequestListener<GifDrawable> {
+        public GlideGifRequestListener() {
+        }
+
+        @Override
+        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
+            return false;
+        }
+
+        @Override
+        public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
+            return false;
+        }
+    }
+
     private static class GlideRequestListener implements RequestListener<Drawable> {
         public GlideRequestListener() {
         }
 
         @Override
-        public boolean onLoadFailed(
-                @Nullable GlideException e,
-                Object model,
-                Target<Drawable> target,
-                boolean isFirstResource) {
+        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
             return false; // Let Glide handle it further
         }
 
         @Override
-        public boolean onResourceReady(
-                Drawable resource,
-                Object model,
-                Target<Drawable> target,
-                com.bumptech.glide.load.DataSource dataSource,
-                boolean isFirstResource) {
+        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
             return false; // Let Glide set the image
         }
     }
